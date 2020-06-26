a9a64a1eda87896f4262badc42df46a5527be66b
vikas0630 <vickyvikas6699@gmail.com>
2020-06-26 14:54:23 +0530

    #7

--- app.py
+++ app.py
@@ -1,43 +1,40 @@
 import os
+
 from flask import Flask, render_template, request
 
-# import our OCR function
-import ocr_core
+from ocr_core import ocr_core
 
-# define a folder to store and later serve the images
 UPLOAD_FOLDER = '/static/uploads/'
-
-# allow files of a specific type
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 
 
-# function to check the file extension
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-# route and function to handle the home page
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
 
-# route and function to handle the upload page
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_page():
     if request.method == 'POST':
-        # check if there is a file in the request
+        # check if the post request has the file part
         if 'file' not in request.files:
             return render_template('upload.html', msg='No file selected')
         file = request.files['file']
-        # if no file is selected
+        # if user does not select file, browser also
+        # submit a empty part without filename
         if file.filename == '':
             return render_template('upload.html', msg='No file selected')
 
         if file and allowed_file(file.filename):
+            file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
+
             # call the OCR function on it
             extracted_text = ocr_core(file)
 
